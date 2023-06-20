4d0ebbb5e5e1844e6c07f0c05dedff9537c24d6e
Stella_Ha <sangyeeh@gmail.com>
2023-06-21 02:41:30 +0900

    :sparkles: counter 생성
    counter 생성

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,10 +1,10 @@
-import "./App.css";
-import Profile from "./components/Profile";
-import Avatar from "./components/Avatar";
+import React from "react";
+import Avatar from "../components/Avatar";
+import Profile from "../components/Profile";
 
-function App() {
+export default function ProfilePage() {
   return (
-    <div className="App">
+    <div>
       <Avatar
         imageUrl={
           "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
@@ -33,5 +33,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
